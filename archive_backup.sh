2d9357557f9c91fbfb24f69bd0d51a88f8dd54a8
Dewnuwan Jayaweera <dewnuwan.jayaweera7@gmail.com>
2025-10-06 22:27:39 +0530

    Added path vars

--- archive_backup.sh
+++ archive_backup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 echo "Archive & Backup"
+read -p "Copy Path: " PATH_TO_COPY
+read -p "Backup Path: " PATH_TO_BACKUP
 
-PATH_TO_COPY=$(read -r "Copy Path")
-PATH_TO_BACUP=$(read -r "Backup Path")
+FILENAME=$(date | grep$)
+
+#echo "$PATH_TO_COPY"
+#echo "$PATH_TO_BACKUP"
+cp -r $PATH_TO_COPY $PATH_TO_BACKUP
+then
+  tar xvf $date
+
+echo ""
+echo "Backup Completed!"
